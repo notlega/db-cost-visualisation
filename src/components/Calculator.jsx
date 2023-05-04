6365ebd2b1569f9e292611bf1f11fe023c16d9e7
Adeeb <adityaadeeb04@gmail.com>
2023-05-04 16:08:34 +0800

    form

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import ReactTooltip from 'react-tooltip';
+
 
 const App = () => {
   const [rows, setRows] = useState(0);
@@ -30,34 +32,127 @@ const App = () => {
     const costPerRow = calculateCostPerRow(table);
     setCost(costPerRow * rows);
   };
+  const styles = {
+    backgroundColor: 'white',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
 
+  const tableStyles = {
+    backgroundColor: 'darkblue',
+    width: '80%',
+    margin: '0 auto',
+    borderCollapse: 'collapse',
+    border: '1px solid white',
+    borderRadius: '8px',
+    overflow: 'hidden',
+  };
+
+  const thStyles = {
+    backgroundColor: 'white',
+    color: 'darkblue',
+    padding: '1rem',
+    textAlign: 'left',
+    borderBottom: '1px solid white',
+    textTransform: 'uppercase',
+    fontSize: '0.8rem',
+    fontWeight: 'bold',
+  };
+
+  const tdStyles = {
+    color: 'white',
+    // make text bold
+    fontWeight: 'bold',
+    padding: '1rem',
+    borderBottom: '1px solid white',
+    fontSize: '0.9rem',
+  };
+
+  const inputStyles = {
+    backgroundColor: 'white',
+    border: 'none',
+    color: 'black',
+    width: '100%',
+    padding: '0.5rem',
+    boxSizing: 'border-box',
+    fontSize: '0.9rem',
+  };
+
+  const buttonStyles = {
+    // button should be at bottom right of the form
+    alignItems: 'center',
+    backgroundColor: 'white',
+    color: 'darkblue',
+    border: 'none',
+    borderRadius: '8px',
+    padding: '1rem 2rem',
+    fontSize: '0.9rem',
+    fontWeight: 'bold',
+    marginTop: '1rem',
+    cursor: 'pointer',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+    transition: 'all 0.3s ease-in-out',
+  };
+  const headerStyle = {
+    textAlign: 'center'
+  }
   return (
     <div>
-      <label htmlFor="table">Table:</label>
-      <select
-        id="table"
-        value={table}
-        onChange={(e) => setTable(e.target.value)}
-      >
-        <option value="">Choose a table</option>
-        <option value="access_token">access_token</option>
-        <option value="advertisements">advertisements</option>
-        {/* Add more options for each table */}
-      </select>
-
-      <label htmlFor="rows">Number of rows:</label>
-      <input
-        id="rows"
-        type="number"
-        value={rows}
-        onChange={(e) => setRows(e.target.value)}
-      />
-
-      <button onClick={calculateCost}>Calculate cost</button>
-
-      <p>Cost: {cost} bits</p>
+      <h1 style={headerStyle}>Database Cost Calculator</h1>
+      <div style={styles}>
+        <form>
+          <table style={tableStyles}>
+            <thead>
+              <tr>
+                <th style={thStyles}>Parameter</th>
+                <th style={thStyles}>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td style={tdStyles} data-tip="Number of companies.">Number of companies (default 200)</td>
+                <td style={tdStyles}><input type="number" defaultValue={200} style={inputStyles} /></td>
+              </tr>
+              <tr>
+                <td style={tdStyles} data-tip="Number of users per company.">Number of users per company (default 1)</td>
+                <td style={tdStyles}><input type="number" defaultValue={1} style={inputStyles} /></td>
+              </tr>
+              <tr>
+                <td style={tdStyles} data-tip="% of users active."> % users active (used to calculate the scale) (default 80%)</td>
+                <td style={tdStyles}><input type="number" defaultValue={80} style={inputStyles} /></td>
+              </tr>
+              <tr>
+                <td style={tdStyles} data-tip="Number of listings per user per year.">Number of listings per user per year (default 10)</td>
+                <td style={tdStyles}><input type="number" defaultValue={10} style={inputStyles} /></td>
+              </tr>
+              <tr>
+                <td style={tdStyles} data-tip="Number of chats opened per listing.">Number of chats opened per listing (default 5)</td>
+                <td style={tdStyles}><input type="number" defaultValue={5} style={inputStyles} /></td>
+              </tr>
+              <tr>
+                <td style={tdStyles} data-tip="Number of messages per chat.">Number of messages per chat (default 10)</td>
+                <td style={tdStyles}><input type="number" defaultValue={10} style={inputStyles} /></td>
+              </tr>
+              <tr>
+                <td style={tdStyles} data-tip="Number of categories.">Number of categories (default 25)</td>
+                <td style={tdStyles}><input type="number" defaultValue={25} style={inputStyles} /></td>
+              </tr>
+              <tr>
+                <td style={tdStyles} data-tip="Number of parameters per category.">Number of Params per category (default 10)</td>
+                <td style={tdStyles}><input type="number" defaultValue={10} style={inputStyles} /></td>
+              </tr>
+            </tbody>
+          </table>
+          <button style={{ ...buttonStyles, marginTop: '2rem' }}>Submit</button>
+        </form>
+      </div>
     </div>
+
+
   );
+
 };
 
 export default App;
