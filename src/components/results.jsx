470d6a91b9313f67a21c395458bda40e7bd45ab9
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-05-04 21:50:15 +0800

    added fonts

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -6,13 +6,13 @@ const Results = ({ result }) => {
     padding: '2rem',
     marginTop: '2rem',
     color: 'white',
-    fontWeight: 'bold',
+    font: 'bold 1.2rem Roboto, sans-serif',
     borderRadius: '0.5rem',
     boxShadow: '0px 0px 10px 1px rgba(0,0,0,0.2)',
   };
 
   const textStyle = {
-    fontSize: '1.2rem',
+    font: 'bold 1.2rem Roboto, sans-serif',
     marginBottom: '1rem',
   };
 
